d19e316cd2d367ce8e85c673e0fcd91173203c9b
BigGreenHat14 <163580586+BigGreenHat14@users.noreply.github.com>
2025-02-22 13:57:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -144,10 +144,10 @@ def bbshell():
     import requests
     version = 0.3
     print(f"Blockbyte Shell v{str(version)}")
-    newversion = float(requests.get("https://raw.githubusercontent.com/BigGreenHat14/BlockByte/refs/heads/main/latestver").text.strip())
+    newversion = float(requests.get("https://pastebin.com/raw/7zMi7wev").text.strip())
     if newversion > version:
         print(f"!!! A new version of the BlockByte server is avaliable, v{str(newversion)} !!!\n(currently v{str(version)})\n")
-    print("Before using, make sure blockbyte is'nt running!")
+    print("Before using, make sure blockbyte isn't running!")
     print("")
     pid = input("Enter project ID > ")
     userbytoken, users = load_data(int(pid))
